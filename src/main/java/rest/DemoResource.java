4eb25c714a6e5ac92f4d5221fdc9c30690eecbca
Theagns <markus.agnsgaard@hotmail.com>
2021-12-07 13:02:12 +0100

    t

--- src/main/java/rest/DemoResource.java
+++ src/main/java/rest/DemoResource.java
@@ -261,7 +261,7 @@ public class DemoResource {
             em.getTransaction().begin();
             // Dropping the table each time because otherwise it may cause multiple entry errors
             em.createNamedQuery("Genre.deleteAllRows").executeUpdate();
-            ScriptUtils.runSQLScript("genreScript.sql", em);
+            ScriptUtils.runSQLScript("genresScript.sql", em);
             em.getTransaction().commit();
         } finally {
             em.close();
